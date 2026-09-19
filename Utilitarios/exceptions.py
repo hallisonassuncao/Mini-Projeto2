6dbaedd93ececa50360424e03bf9cb2f5845f723
hallisonassuncao <hallison_assuncao>
2026-09-19 16:55:02 -0300

    Aplicação Full-Stack de Sistema Bancário

--- Utilitarios/exceptions.py
+++ Utilitarios/exceptions.py
@@ -1,4 +1,3 @@
-# Data Science Academy
 # Mini-Projeto 2 - Aplicação Full-Stack de Sistema Bancário em Python com Programação Orientada a Objetos
 # Módulo para exceções customizadas da aplicação.
 
